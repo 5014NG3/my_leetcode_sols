9d4688d9bea7a0dee661fcc4438d9b601e03623a
root <root@bby-1337.localdomain>
2023-01-17 18:22:08 -0800

    Add ~optimal solution to merged k lists

--- merge_k_lists_23.cpp
+++ merge_k_lists_23.cpp
@@ -72,3 +72,89 @@ public:
     }
 
 };
+
+
+
+//////////////////////////////`````````````~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//optimal solution
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+// #include<bits/stdc++.h>
+
+
+
+// class compare{
+//     public:
+//       bool operator()(ListNode* a,ListNode* b)
+//       {
+//          return a->val > b->val;
+//       }
+// };
+
+// class Solution {
+// public:
+//     ListNode* mergeKLists(vector<ListNode*>& lists) {
+//         priority_queue<ListNode*,vector<ListNode*>,compare> pq;
+//         if(lists.size()==0) return nullptr;
+//         for(int i=0;i<lists.size();i++)
+//         {
+
+               //pushing the first node in the list to priority queue based
+               //by comparing the values between the head of the different lists
+               //only added to the priority queue if it is not null
+//             if(lists[i]) pq.push(lists[i]);
+//         }
+//         ListNode* head=nullptr;
+//         ListNode* tail=nullptr;
+
+//         while(!pq.empty())
+//         {
+//             ListNode* temp=pq.top();
+//             pq.pop();
+                // get the first value from the priority quueu, temp
+                // get the rest of the values from this list
+                //and push into the priority queue, order is preserved
+
+//             if(temp->next) pq.push(temp->next);
+
+                //this is the starting point, before head was equal to nullptr
+//             if(head==nullptr) {
+
+                   //head is now the current smallest value
+//                 head=temp;
+
+                    //tail is equal to head from here on out
+//                 tail=head;
+                
+//             }
+
+                //head != nullptr
+//             else{
+
+                //before this head == tail;
+                //"append" values to the list
+
+//              tail->next=temp;
+
+                //tail is now the current value
+//              tail=temp;
+
+                //the head isn't really important
+                //we are just adding values to the tail
+                //until the pq is empty
+             
+//             }
+//         }
+        
+//        return head;
+//     }
+// };
